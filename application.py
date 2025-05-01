212d29cd5d0f7f190c4805b4dc81a38be89e70ad
Michael Black <wmblack23@gmail.com>
2025-05-01 00:27:16 -0600

    Index file and application.py - forgot to make a branch

---
+++ application.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template, redirect, url_for, flash, session, make_response, jsonify
+from flask_cors import CORS
+from dotenv import load_dotenv
+from pymongo import MongoClient
+import os
+
+load_dotenv()
+
+application = Flask(__name__)
+CORS(application)
+
+# Connect to Mongo
+clusterUrl = os.environ["clusterUrl"]
+client = MongoClient(clusterUrl)
+db = client['latAm-app']
+
+@application.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    application.run(debug=True)
